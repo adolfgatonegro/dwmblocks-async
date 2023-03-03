11d7eb3b0f99ae36a1539d4e1e95af97e459f838
adolfgatonegro <github@gatoneg.ro>
2023-03-02 19:06:07 -0600

    add mpris script

--- config.h
+++ config.h
@@ -1,9 +1,11 @@
-#define CMDLENGTH 45
+#define CMDLENGTH 50
 #define DELIMITER "  "
 #define LEADING_DELIMITER
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
+	BLOCK("sb-mpris", 5, 13),
+	BLOCK("sb-updates", 21600, 12),
 	BLOCK("sb-volume", 0, 10),
 	BLOCK("sb-clock", 30, 11),
 };
